e4dad2e3f9b297718f5f8bc37fb8d0a3c9332f3a
yangshen <uQES0irbJDs>
2019-04-23 17:23:32 +0800

    日志打印上传

--- src/main/java/com/goudaner/platform/controller/TestController.java
+++ src/main/java/com/goudaner/platform/controller/TestController.java
@@ -1,15 +1,19 @@
 package com.goudaner.platform.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     private String from = "1111";
 
     @RequestMapping("/fromss")
     public String from(){
+        logger.info("12313");
         return this.from;
     }
 }
