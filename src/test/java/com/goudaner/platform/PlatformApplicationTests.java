e51b14431d8b38d4d2f77c8727053b38435dbba2
yangshen <uQES0irbJDs>
2019-05-07 10:48:12 +0800

    启动不起来 啊。。。。

--- src/test/java/com/goudaner/platform/PlatformApplicationTests.java
+++ src/test/java/com/goudaner/platform/PlatformApplicationTests.java
@@ -3,9 +3,15 @@ package com.goudaner.platform;
 import com.goudaner.platform.dto.GdAccountDto;
 import com.goudaner.platform.entity.GdAccount;
 import com.goudaner.platform.mapper.GdAccountMapper;
+import com.goudaner.platform.orderStateMachine.FSMBuilder;
+import com.goudaner.platform.orderStateMachine.OrderEvent;
+import com.goudaner.platform.orderStateMachine.OrderStates;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
@@ -14,8 +20,22 @@ import java.util.Date;
 @SpringBootTest
 public class PlatformApplicationTests {
 
+    @Autowired
+    private BeanFactory beanFactory;
+
+    @Autowired
+    private FSMBuilder sSMBuilder;
     @Test
     public void contextLoads() {
+        StateMachine<OrderStates, OrderEvent> machine = null;
+        try {
+            machine = sSMBuilder.initMachine(beanFactory);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        machine.sendEvent(OrderEvent.PAY);
+        System.out.println("过了一会");
+        machine.sendEvent(OrderEvent.PAY);
         //given
 //        GdAccount account = GdAccount.builder().systemNo("1111").accountNo("22222").accountName("33333").gmtModify(new Date()).build();
 //
